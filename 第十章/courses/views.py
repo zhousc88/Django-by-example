a06b30250f2c00e21e982b5ac3affe2c61a5aad2
zhousc88 <zhousc88@gmail.com>
2018-04-02 22:29:19 +0800

    Tenth

--- 第十章/courses/views.py
+++ 第十章/courses/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from  .models import Course
+from django.views.generic.list import ListView
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.contrib.auth.decorators import permission_required,PermissionDenied
 
 # Create your views here.
+'''
+class ManageCourseListView(ListView):
+    model = Course
+    template_name = 'courses/manage/course/list.html'
+    def get_queryset(self):
+        qs = super(ManageCourseListView,self).get_queryset()
+        return qs.filter(owner=self.request.user)
+'''
+class OwnerMixin(object):
+    def get_queryset(self):
+        qs = super(OwnerMixin,self).get_queryset()
+        return qs.filter(owner=self.request.user)
+
+class OwnerEditMixin(object):
+    def form_valid(self,form):
+        form.instance.owner = self.request.user
+        return super(OwnerEditMixin,self).form_valid(form)
+
+class OwnerCourseMixin(OwnerMixin):
+    model =Course
+
+class OwnerCourseEditMixin(OwnerCourseMixin,OwnerEditMixin):
+    fields = ['subject','title','slug','overview']
+    success_url = reverse_lazy('manage_course_list')
+    template_name = 'courses/manage/course/form.html'
+
+class ManageCourseListView(OwnerCourseMixin,ListView):
+    template_name = 'courses/manage/course/list.html'
+
+class CourseCreateView(OwnerCourseEditMixin,CreateView):
+    pass
+
+class CourseUpdateView(OwnerCourseEditMixin,UpdateView):
+    pass
+
+class CourseDeleteView(OwnerCourseMixin,DeleteView):
+    template_name = 'course/manage/course/delete.html'
+    success_url = reverse_lazy('manage_course_list')
+
