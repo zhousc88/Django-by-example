ce115495f1e9a0eb4125740f2a1053394b079ada
zhousc <zhousc88@gmail.com>
2018-04-04 17:39:23 +0800

    twelfth
    API

---
+++ 第十二章/courses/api/views.py
@@ -0,0 +1,24 @@
+from rest_framework import generics
+from ..models import Subject,Course
+from .serializers import SubjectSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.shortcuts import  get_object_or_404
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+class SubjectListView(generics.ListAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+
+class SubjectDetailView(generics.RetrieveAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+
+class CourseEnrollView(APIView):
+    authentication_classes = (BasicAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    def post(self,request,pk,format=None):
+        course = get_object_or_404(Course,pk=pk)
+        course.students.add(request.user)
+        return  Response({'enrolled':True})
